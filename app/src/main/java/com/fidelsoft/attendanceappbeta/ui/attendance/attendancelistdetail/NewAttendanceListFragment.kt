92d18b59cca6c41c37ad5b12444871dd5b47149f
AkiyoDesktop <akiyofidel@gmail.com>
2023-05-06 22:26:04 +0300

    Added list view for meeting and notes listings

---
+++ app/src/main/java/com/fidelsoft/attendanceappbeta/ui/attendance/attendancelistdetail/NewAttendanceListFragment.kt
@@ -0,0 +1,32 @@
+package com.fidelsoft.attendanceappbeta.ui.attendance.attendancelistdetail
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.fidelsoft.attendanceappbeta.R
+
+class NewAttendanceListFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = NewAttendanceListFragment()
+    }
+
+    private lateinit var viewModel: NewAttendanceListViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_new_attendance_list, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(NewAttendanceListViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/fidelsoft/attendanceappbeta/ui/attendance/attendancelistdetail/NewAttendanceListViewModel.kt
@@ -0,0 +1,7 @@
+package com.fidelsoft.attendanceappbeta.ui.attendance.attendancelistdetail
+
+import androidx.lifecycle.ViewModel
+
+class NewAttendanceListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
